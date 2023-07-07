631faa95e9e3fd6555c85a6cda1455e0f36bed4e
yifei-yao <yaoyifei0@gmail.com>
2023-07-07 14:07:43 -0400

    Update dummy.py

--- lambda_functions/dummy.py
+++ lambda_functions/dummy.py
@@ -4,8 +4,8 @@ import datetime
 import uuid
 
 session_id = str(uuid.uuid4())
-
 lex_client = boto3.client("lexv2-runtime")
+sqs = boto3.client("sqs")
 
 
 def lambda_handler(event, context):
@@ -23,31 +23,68 @@ def lambda_handler(event, context):
             sessionId=session_id,
             text=user_message,
         )
+        sessionState = response.get("sessionState", {})
+        if sessionState is not None:
+            send_slots(sessionState)
         messages = response.get("messages", [])
-        bot_responses = [
-            {
-                "type": "response",
-                "unstructured": {
-                    "id": str(i + 1),
-                    "text": msg.get("content", ""),
-                    "timestamp": str(datetime.datetime.now()),
-                },
-            }
-            for i, msg in enumerate(messages)
-        ]
-
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-            },
-            "body": json.dumps({"messages": bot_responses}),
-        }
+        if messages is not None:
+            return pack_up(messages)
+        else:
+            return error("No message returned")
     else:
-        return {
-            "statusCode": 400,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
+        return error("Invalid request format")
+
+
+def send_slots(sessionState):
+    intent = sessionState.get("intent", {})
+    if intent is None:
+        return
+    if (
+        intent["name"] == "DiningSuggestion"
+        and intent["state"] == "ReadyForFulfillment"
+    ):
+        slots = intent["slots"]
+        if not check_filled(slots):
+            return
+        sqs.send_message(
+            QueueUrl="https://sqs.us-east-1.amazonaws.com/466369883393/UserRequest",
+            MessageBody=json.dumps(slots),
+        )
+
+
+def check_filled(slots):
+    for value in slots.values():
+        if value is None:
+            return False
+    return True
+
+
+def pack_up(messages):
+    bot_responses = [
+        {
+            "type": "response",
+            "unstructured": {
+                "id": str(i + 1),
+                "text": msg.get("content", ""),
+                "timestamp": str(datetime.datetime.now()),
             },
-            "body": json.dumps({"error": "Invalid request format"}),
         }
+        for i, msg in enumerate(messages)
+    ]
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps({"messages": bot_responses}),
+    }
+
+
+def error(message):
+    return {
+        "statusCode": 400,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps({"error": message}),
+    }
