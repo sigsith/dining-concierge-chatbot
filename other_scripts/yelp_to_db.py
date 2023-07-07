1c1d5df4a90058f7a20048d60307d626ac9cdcf2
yifei-yao <yaoyifei0@gmail.com>
2023-07-07 12:04:31 -0400

    Update yelp_to_db.py

--- other_scripts/yelp_to_db.py
+++ other_scripts/yelp_to_db.py
@@ -39,42 +39,45 @@ cuisines = [
 ]
 
 for cuisine in cuisines:
-    url = (
-        "https://api.yelp.com/v3/businesses/search?location=Manhattan&term="
-        + cuisine
-        + "+restaurants"
-    )
+    counter = 0
     print(f"Getting data for {cuisine}")
-    response = requests.get(url, headers=headers)
+    for offset in range(0, 1000, 50):
+        url = (
+            "https://api.yelp.com/v3/businesses/search?location=Manhattan&term="
+            + cuisine
+            + "+restaurants"
+            + f"&limit=50&offset={offset}"
+        )
+        response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        businesses = response.json()["businesses"]
-        counter = 0
-        print(f"Received {len(businesses)} businesses for {cuisine}")
-        for business in businesses:
-            counter += 1
-            if counter > 1000:
-                break
-            item = {
-                "Business ID": business["id"],
-                "Name": business["name"],
-                "Address": " ".join(business["location"]["display_address"]),
-                "Coordinates": {
-                    "latitude": Decimal(str(business["coordinates"]["latitude"])),
-                    "longitude": Decimal(str(business["coordinates"]["longitude"])),
-                },
-                "Number of Reviews": business["review_count"],
-                "Rating": Decimal(str(business["rating"])),
-                "Zip Code": business["location"]["zip_code"],
-                "insertedAtTimestamp": datetime.now().isoformat(),
-            }
-            print(f"Inserting item {counter} for {cuisine}")
-            # insert the item into dynamodb
-            try:
-                table.put_item(Item=item)
-                print(f"Successfully inserted item {counter} for {cuisine}")
-            except Exception as e:
-                print(f"Failed to insert item {counter} for {cuisine}. Error: {e}")
-    else:
-        print(f"Request failed with status code {response.status_code}")
-    time.sleep(1)
+        if response.status_code == 200:
+            businesses = response.json()["businesses"]
+            print(f"Received {len(businesses)} businesses for {cuisine}")
+            for business in businesses:
+                counter += 1
+                if counter > 1000:
+                    break
+                item = {
+                    "Business ID": business["id"],
+                    "Name": business["name"],
+                    "Address": " ".join(business["location"]["display_address"]),
+                    "Coordinates": {
+                        "latitude": Decimal(str(business["coordinates"]["latitude"])),
+                        "longitude": Decimal(str(business["coordinates"]["longitude"])),
+                    },
+                    "Number of Reviews": business["review_count"],
+                    "Rating": Decimal(str(business["rating"])),
+                    "Zip Code": business["location"]["zip_code"],
+                    "insertedAtTimestamp": datetime.now().isoformat(),
+                }
+                print(f"Inserting item {counter} for {cuisine}")
+                # insert the item into dynamodb
+                try:
+                    table.put_item(Item=item)
+                    print(f"Successfully inserted item {counter} for {cuisine}")
+                except Exception as e:
+                    print(f"Failed to insert item {counter} for {cuisine}. Error: {e}")
+
+        else:
+            print(f"Request failed with status code {response.status_code}")
+        time.sleep(1)
